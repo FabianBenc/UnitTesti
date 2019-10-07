ec0adf23f050bc7bc07d9364946b5c8f0050b96c
Fabian Benc <fbenc@omega-software.hr>
2019-10-07 12:21:28 +0200

    HashTablePrimjer

--- ZadatakGIT/Program.cs
+++ ZadatakGIT/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace ZadatakGIT
 {
@@ -66,6 +67,23 @@ namespace ZadatakGIT
         }
         public void HashTablePrimjer()
         {
+            Hashtable tablica = new Hashtable();
+
+            tablica.Add(1, "Ivica");
+            tablica.Add(2, "Marko");
+            tablica.Add(3, "Ivan");
+            tablica.Add(4, "Marija");
+
+            Console.WriteLine("Broj elemenata u hash tablici:{0}", tablica.Count);
+            Console.WriteLine("Ispis elementa pomocu kljuca:{0}", tablica[2]);
+            Console.WriteLine("Ispis hash tablice");
+
+            foreach (DictionaryEntry entry in tablica)
+            {
+                Console.WriteLine("{0} {1}", entry.Key, entry.Value);
+            }
+
+            Console.WriteLine("Postoji li kljuc:{0}", tablica.Contains(4));
 
         }
     }
