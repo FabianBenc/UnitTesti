609c4ff47c2427109459636893882de5d0ed34dd
Fabian Benc <fbenc@omega-software.hr>
2019-12-04 16:13:29 +0100

    fix

--- HoteliTest/MockData/MockHotel_Data.cs
+++ HoteliTest/MockData/MockHotel_Data.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MockData
 {
-    class MockHotel_Data
+    public static class MockHotel_Data
     {
+        public static void setupSet<T>(ICollection<T> data, Mock<DbSet<T>> set) where T : class
+        {
+            var dataQ = data.AsQueryable<T>();
+            set.As<IQueryable<T>>().Setup(m => m.Provider).Returns(() => dataQ.Provider);
+        }
     }
 }
